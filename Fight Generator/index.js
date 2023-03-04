284ca37dd6eee9f66f5f2b437328ff6ba81936ce
gem-jayash <jayash.yadav@geminisolutions.com>
2023-03-04 19:29:58 +0530

    fight generator

---
+++ Fight Generator/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+const supervillains = require('supervillains');
+ 
+var sv = supervillains.random();
+var sh = superheroes.random();
+
+console.log(sh+" VS "+sv);
